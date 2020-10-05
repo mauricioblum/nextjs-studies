a703a8fae7fdf74c8833483b2166a31b644ea887
Mauricio Blum <mauricio.blum@itsector.pt>
2020-10-05 11:21:44 +0100

    ts paths 404, dynamics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
-import { Title } from '../styles/pages/Home';
+import { resolveSoa } from 'dns';
+import { GetServerSideProps } from 'next';
+import { Title } from '@/styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>Hello world</Title>
+      <section>
+        <Title>Hello world</Title>
+
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => {
+            return (
+              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts,
+    },
+  };
+};
